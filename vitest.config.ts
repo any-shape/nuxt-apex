065e139c56f00ecb4ebe5b88366c6772b1bce81c
Antony Mik <anmik13@gmail.com>
2025-07-23 23:58:36 +0300

    update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,11 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
     projects: [{
         test: {
-          root: 'playground',
+          root: resolve(__dirname, 'playground'),
           include: ['../test/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
           name: 'ci',
           environment: 'node',
@@ -12,3 +13,4 @@ export default defineConfig({
     }]
   }
 })
+
