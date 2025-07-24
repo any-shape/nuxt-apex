004dbcd2dbe1fbf3936ae3f9a03398b240980a1c
An Mik <anmik13@gmail.com>
2025-07-24 08:35:42 +0300

    update vitesttest file

--- test/codegen.spec.ts
+++ test/codegen.spec.ts
@@ -6,40 +6,40 @@ import { Project } from 'ts-morph'
 import { readFile } from 'node:fs/promises'
 import { glob } from 'tinyglobby'
 
-const apiDir = resolve(import.meta.dirname, '../playground/server', DEFAULTS.sourcePath).replace(/\\/g, '/')
-const outputDir = resolve(import.meta.dirname, '../playground', DEFAULTS.outputPath).replace(/\\/g, '/')
-const templateFile = resolve(import.meta.dirname, '../src/runtime/templates/fetch.txt').replace(/\\/g, '/')
+const root = process.cwd()
+const apiDir = resolve(root, './playground/server', DEFAULTS.sourcePath).replace(/\\/g, '/')
+const outputDir = resolve(root, './playground', DEFAULTS.outputPath).replace(/\\/g, '/')
+const templateFile = resolve(root, './src/runtime/templates/fetch.txt').replace(/\\/g, '/')
 const endpoints = (await glob(apiDir + '/**/*.(get|post|put|delete).ts', { cwd: apiDir, absolute: true })).map(e => e.replace(/\\/g, '/'))
 
-console.log(apiDir, outputDir, templateFile);
 
-// describe('Test type extraction and composable code generation', () => {
-//   const tsProject = new Project({ tsConfigFilePath: resolve(apiDir, '../tsconfig.json').replace(/\\/g, '/'),
-//     skipFileDependencyResolution: true,
-//     compilerOptions: {
-//       skipLibCheck: true,
-//       allowJs: false,
-//       declaration: false,
-//       noEmit: true,
-//       preserveConstEnums: false,
-//     }
-//   })
+describe('Test type extraction and composable code generation', () => {
+  const tsProject = new Project({ tsConfigFilePath: resolve(apiDir, '../tsconfig.json').replace(/\\/g, '/'),
+    skipFileDependencyResolution: true,
+    compilerOptions: {
+      skipLibCheck: true,
+      allowJs: false,
+      declaration: false,
+      noEmit: true,
+      preserveConstEnums: false,
+    }
+  })
 
-//   beforeEach(() => {
-//     vol.reset()
-//   })
+  beforeEach(() => {
+    vol.reset()
+  })
 
 
-//   for (const endpoint of endpoints) {
-//     it(`generates a composable for endpoint: ${endpoint.split(apiDir)[1]}`, async () => {
-//       const et = await extractTypesFromEndpoint(endpoint, tsProject, DEFAULTS.serverEventHandlerName, false)
-//       const es = getEndpointStructure(endpoint, apiDir, 'api')
-//       const code = constructComposableCode(await readFile(templateFile, 'utf-8'), et, es, DEFAULTS.composableName)
+  for (const endpoint of endpoints) {
+    it(`generates a composable for endpoint: ${endpoint.split(apiDir)[1]}`, async () => {
+      const et = await extractTypesFromEndpoint(endpoint, tsProject, DEFAULTS.serverEventHandlerName, false)
+      const es = getEndpointStructure(endpoint, apiDir, DEFAULTS.sourcePath)
+      const code = constructComposableCode(await readFile(templateFile, 'utf-8'), et, es, DEFAULTS.composableName)
 
-//       const path = resolve(outputDir, './composables', `${DEFAULTS.composableName + es.name}.ts`).replace(/\\/g, '/')
-//       vol.fromJSON({ [path]: code })
+      const path = resolve(outputDir, './composables', `${DEFAULTS.composableName + es.name}.ts`).replace(/\\/g, '/')
+      vol.fromJSON({ [path]: code })
 
-//       expect(code).toMatchSnapshot()
-//     })
-//   }
-// })
+      expect(code).toMatchSnapshot()
+    })
+  }
+})
