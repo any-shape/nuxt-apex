a0bbe3574509d9646b95c38ded67dd65a0533821
An Mik <anmik13@gmail.com>
2025-07-24 08:54:26 +0300

    ..

--- test/see.ts
+++ test/see.ts
@@ -1,8 +1,10 @@
 import { resolve } from 'node:path'
+import { glob } from 'tinyglobby'
 
 const root = process.cwd()
-const apiDir = resolve(root, './playground/server').replace(/\\/g, '/')
+const apiDir = resolve(root, './playground/server/api').replace(/\\/g, '/')
 const outputDir = resolve(root, './playground').replace(/\\/g, '/')
 const templateFile = resolve(root, './src/runtime/templates/fetch.txt').replace(/\\/g, '/')
+const endpoints = (await glob(apiDir + '/**/*.(get|post|put|delete).ts', { cwd: apiDir, absolute: true })).map(e => e.replace(/\\/g, '/'))
 
-console.log(apiDir, outputDir, templateFile);
+console.log(endpoints);
