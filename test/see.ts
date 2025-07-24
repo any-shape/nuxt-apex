dc7a6b6c9c31559fa40e356bec044f469a5c86ea
An Mik <anmik13@gmail.com>
2025-07-24 08:51:43 +0300

    ..

--- test/see.ts
+++ test/see.ts
@@ -1,9 +1,8 @@
-import { DEFAULTS } from '../src/module.ts'
 import { resolve } from 'node:path'
 
 const root = process.cwd()
-const apiDir = resolve(root, './playground/server', DEFAULTS.sourcePath).replace(/\\/g, '/')
-const outputDir = resolve(root, './playground', DEFAULTS.outputPath).replace(/\\/g, '/')
+const apiDir = resolve(root, './playground/server').replace(/\\/g, '/')
+const outputDir = resolve(root, './playground').replace(/\\/g, '/')
 const templateFile = resolve(root, './src/runtime/templates/fetch.txt').replace(/\\/g, '/')
 
 console.log(apiDir, outputDir, templateFile);
