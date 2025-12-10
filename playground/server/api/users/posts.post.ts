3c1cbb0d1d5496ba577433e0c092de49ebc3a030
Antony <anmik13@gmail.com>
2025-12-10 13:19:51 +0200

    feat: core update (#29)
    Update deps to nuxt 4!
    Hide the composables under .nuxt folder with autoimport preserving;
    Fix varning on 'tsconfig.json' not found while startup;
    Fix error while endpoint removng;
    Add fallback when input type is undefined;
    Allow composable function to take no params instead of empty object.

--- playground/server/api/users/posts.post.ts
+++ playground/server/api/users/posts.post.ts
@@ -2,5 +2,5 @@ import {Input21} from '~/server/api/types.d';
 import { randomData4 } from './../utils/utils2';
 
 export default defineApexHandler<Input21>(async (data) => {
-  return randomData4(data);
+  return randomData4(data)
 });
